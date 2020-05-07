39d5049865784b845ebd37cf78d8353cd5984221
rudenick0309 <rudenick0309@gmail.com>
2020-05-07 22:52:57 +0900

    2까지

--- models/user2.js
+++ models/user2.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
 				type: DataTypes.STRING,
 				allowNull: false,
 			},
+			email: {
+				type: DataTypes.STRING,
+				allowNUll: false,
+			},
 		},
 		{}
 	);
